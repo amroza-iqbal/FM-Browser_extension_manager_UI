fb92691689e6e94cf02e5739c961f8988bb977f1
amroza-iqbal <mona.foxylady@gmail.com>
2025-07-11 19:04:18 +0500

    first changes

--- js/script.js
+++ js/script.js
@@ -37,22 +37,59 @@ themeswitch.addEventListener("click", () =>
 
 
 
+        const allButton = document.getElementById("allButton");
         const activeButton = document.getElementById("activeButton");
         const inactiveButton = document.getElementById("inactiveButton");
         const activeExtensions = document.querySelectorAll(".active-extension");
         const inactiveExtensions = document.querySelectorAll(".inactive-extension");
 
+        allButton.addEventListener("click", function()
+        {
+            inactiveExtensions.forEach(extension => extension.style.display = "block");
+            activeExtensions.forEach(extension => extension.style.display = "block");
+            inactiveButton.classList.remove('active');
+            activeButton.classList.remove('active');
+            allButton.classList.add('active');
+        });
+
         activeButton.addEventListener("click", function ()
         {
             inactiveExtensions.forEach(extension => extension.style.display = "none");
             activeExtensions.forEach(extension => extension.style.display = "block");
+            allButton.classList.remove('active');
+            inactiveButton.classList.remove('active');
+            activeButton.classList.add('active');
         });
 
         inactiveButton.addEventListener("click", function ()
         {
             activeExtensions.forEach(extension => extension.style.display = "none");
             inactiveExtensions.forEach(extension => extension.style.display = "block");
-            inactiveExtensions.classList.add('.active');
+            allButton.classList.remove('active');
+            activeButton.classList.remove('active');
+            inactiveButton.classList.add('active');
+        });
+
+
+
+        const sliders = document.querySelectorAll('.switch input');
+
+        sliders.forEach(slider =>
+        {
+            slider.addEventListener('change', function()
+            {
+                const box =  this.closest('.box');
+                if (this.checked)
+                {
+                    box.classList.add('active-ext');
+                    box.classList.add('inactive-ext');
+                }
+                else
+                {
+                    box.classList.remove('active-ext');
+                    box.classList.add('inactive-ext');
+                }
+            });
         });
 
 
